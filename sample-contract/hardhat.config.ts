b9f182fbf6bb8f4695c98a1aa68cf833796902de
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-09-24 21:39:08 +0900

    追記

--- sample-contract/hardhat.config.ts
+++ sample-contract/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    "mantle-testnet": {
+      url: "https://rpc.testnet.mantle.xyz/",
+      accounts: [process.env.PRIV_KEY] 
+    }
+  }
 };
 
 export default config;
